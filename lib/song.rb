795136e6190a30162d2d72042b7bf8a0b9200943
Ranika Williams <rnkprice07@gmail.com>
2019-05-31 18:03:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,25 +10,25 @@ class Song
     self.class.all << self
   end
 
-  def self.create
+  def self.create #Class Constructor
     song = self.new
     self.all << song
     song
   end
 
-  def self.new_by_name(song_title)
+  def self.new_by_name(song_title) #Class Constructor
     song = self.new
     song.name = song_title
     song
   end
 
-  def self.create_by_name(song_title)
+  def self.create_by_name(song_title) #Class Constructor
     song = self.create
     song.name = song_title
     song
   end
 
-  def self.find_by_name(song_title)
-    result = self.all
+  def self.find_by_name(song_title) #Class Finder
+    result = self.all.detect {|song| song.name == title}
   end
 end
